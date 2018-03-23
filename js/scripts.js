660c292ba7290b9bde2ed6740580e02eb68f3cb0
Jimmy McNamara <jmcnamara44@gmail.com>
2018-03-23 13:30:09 -0700

    style html elements.

--- js/scripts.js
+++ js/scripts.js
@@ -16,33 +16,33 @@ $(document).ready(function() {
 
   });
 
-  // $("#trackChooser").submit(function(event) {
-  //   event.preventDefault();
-  //   var nameInput = $("#name").val();
-  //   var picture = $("input:radio[name=picture]:checked").val();
-  //   $(".name").text(nameInput);
-  //
-  //   if (nameInput) {
-  //     $(".error").hide();
-  //
-  //     if (picture === "lake") {
-  //       $(".results").hide();
-  //       $("#ruby").show();
-  //     } else if (picture === "mountain"){
-  //       $(".results").hide();
-  //       $("#cSharp").show();
-  //     } else if (picture === "bridge"){
-  //       $(".results").hide();
-  //       $("#java").show();
-  //     } else  if (picture === "field"){
-  //       $(".results").hide();
-  //       $("#css").show();
-  //     } else {
-  //       $("#notComplete").show();
-  //     }
-  //
-  //   } else {
-  //     $(".error").show();
-  //   }
-  // });
+  $("#trackChooser").submit(function(event) {
+    event.preventDefault();
+    var nameInput = $("#name").val();
+    var picture = $("input:radio[name=picture]:checked").val();
+    $(".name").text(nameInput);
+
+    if (nameInput) {
+      $(".error").hide();
+
+      if (picture === "lake") {
+        $(".results").hide();
+        $("#ruby").show();
+      } else if (picture === "mountain"){
+        $(".results").hide();
+        $("#cSharp").show();
+      } else if (picture === "bridge"){
+        $(".results").hide();
+        $("#java").show();
+      } else  if (picture === "field"){
+        $(".results").hide();
+        $("#css").show();
+      } else {
+        $("#notComplete").show();
+      }
+
+    } else {
+      $(".error").show();
+    }
+  });
 });
