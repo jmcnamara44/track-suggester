38fc92c31502a1db7c241fdefeb82b23f79bb952
Jimmy McNamara <jmcnamara44@gmail.com>
2018-03-23 10:59:54 -0700

    include functionality to show and hide track results based on picture question.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,19 @@ $(document).ready(function() {
   $("#trackChooser").submit(function(event) {
     event.preventDefault();
     var nameInput = $("#name").val();
+    var picture = $("input:radio[name=picture]:checked").val();
 
-
-    if (nameInput) {
-
-    } else {
-      $(".error").show();
-    }
+    // if (nameInput) {
+    //   $(".error").hide();
+      if (picture === "lake") {
+        $(".results").hide();
+        $("#ruby").show();
+      } else {
+        $(".results").hide();
+        $("#cSharp").show();
+      }
+    // } else {
+    //   $(".error").show();
+    // }
   });
 });
