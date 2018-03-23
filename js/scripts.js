3e151b972cd94c5df24df44daa7f94cf0a6c5850
Jimmy McNamara <jmcnamara44@gmail.com>
2018-03-23 13:20:16 -0700

    ask the user to select which epicodus campus they will be enrolling at, and based off this direct the user to the survey.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,48 @@
 $(document).ready(function() {
-  $("#trackChooser").submit(function(event) {
+
+  $("#portland").click(function(event) {
     event.preventDefault();
-    var nameInput = $("#name").val();
-    var picture = $("input:radio[name=picture]:checked").val();
-    $(".name").text(nameInput);
+    $("#location").hide();
+    $(".epicodus").show();
+    $(".portland").show();
 
-    if (nameInput) {
-      $(".error").hide();
-
-      if (picture === "lake") {
-        $(".results").hide();
-        $("#ruby").show();
-      } else if (picture === "mountain"){
-        $(".results").hide();
-        $("#cSharp").show();
-      } else if (picture === "bridge"){
-        $(".results").hide();
-        $("#java").show();
-      } else  if (picture === "field"){
-        $(".results").hide();
-        $("#css").show();
-      } else {
-        $("#notComplete").show();
-      }
-
-    } else {
-      $(".error").show();
-    }
   });
+
+  $("#seattle").click(function(event) {
+    event.preventDefault();
+    $("#location").hide();
+    $(".epicodus").show();
+    $(".seattle").show();
+
+  });
+
+  // $("#trackChooser").submit(function(event) {
+  //   event.preventDefault();
+  //   var nameInput = $("#name").val();
+  //   var picture = $("input:radio[name=picture]:checked").val();
+  //   $(".name").text(nameInput);
+  //
+  //   if (nameInput) {
+  //     $(".error").hide();
+  //
+  //     if (picture === "lake") {
+  //       $(".results").hide();
+  //       $("#ruby").show();
+  //     } else if (picture === "mountain"){
+  //       $(".results").hide();
+  //       $("#cSharp").show();
+  //     } else if (picture === "bridge"){
+  //       $(".results").hide();
+  //       $("#java").show();
+  //     } else  if (picture === "field"){
+  //       $(".results").hide();
+  //       $("#css").show();
+  //     } else {
+  //       $("#notComplete").show();
+  //     }
+  //
+  //   } else {
+  //     $(".error").show();
+  //   }
+  // });
 });
