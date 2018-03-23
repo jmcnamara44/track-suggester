4f2b8bfed83486d52f58733595b729bbd4bfd1e7
Jimmy McNamara <jmcnamara44@gmail.com>
2018-03-23 11:29:12 -0700

    show an error message if the picture question is not answered and submit button is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,29 @@ $(document).ready(function() {
     event.preventDefault();
     var nameInput = $("#name").val();
     var picture = $("input:radio[name=picture]:checked").val();
+    $(".name").text(nameInput);
 
-    // if (nameInput) {
-    //   $(".error").hide();
+    if (nameInput) {
+      $(".error").hide();
+
       if (picture === "lake") {
         $(".results").hide();
         $("#ruby").show();
-      } else {
+      } else if (picture === "mountain"){
         $(".results").hide();
         $("#cSharp").show();
+      } else if (picture === "bridge"){
+        $(".results").hide();
+        $("#java").show();
+      } else  if (picture === "field"){
+        $(".results").hide();
+        $("#css").show();
+      } else {
+        $("#notComplete").show();
       }
-    // } else {
-    //   $(".error").show();
-    // }
+
+    } else {
+      $(".error").show();
+    }
   });
 });
